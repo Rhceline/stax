439567e294b248d57512d1ed914600f0ebbe82ad
Rhceline <celine.rexharrison@gmail.com>
2022-04-21 23:22:42 +0200

    site stax

---
+++ assets/javascript/app.js
@@ -0,0 +1,286 @@
+/* <------------------------------------> */
+/* -- POPUP -- */
+/* <------------------------------------> */
+
+$(function () {
+  // JU
+  $(".btnpopupju").on("click", function () {
+    $(".popupju").css("display", "flex");
+  });
+
+  $(".popupju .exit").on("click", function () {
+    $(".popupju").css("display", "none");
+  });
+
+  // ELZA
+  $(".btnpopupelza").on("click", function () {
+    $(".popupelza").css("display", "flex");
+  });
+
+  $(".popupelza .exit").on("click", function () {
+    $(".popupelza").css("display", "none");
+  });
+
+  // BIBI
+  $(".btnpopupbibi").on("click", function () {
+    $(".popupbibi").css("display", "flex");
+  });
+
+  $(".popupbibi .exit").on("click", function () {
+    $(".popupbibi").css("display", "none");
+  });
+
+  // ZINA
+  $(".btnpopupzina").on("click", function () {
+    $(".popupzina").css("display", "flex");
+  });
+
+  $(".popupzina .exit").on("click", function () {
+    $(".popupzina").css("display", "none");
+  });
+
+  // BIBI
+  $(".btnpopupthomas").on("click", function () {
+    $(".popupthomas").css("display", "flex");
+  });
+
+  $(".popupthomas .exit").on("click", function () {
+    $(".popupthomas").css("display", "none");
+  });
+
+  // CECE
+  $(".btnpopupcece").on("click", function () {
+    $(".popupcece").css("display", "flex");
+  });
+
+  $(".popupcece .exit").on("click", function () {
+    $(".popupcece").css("display", "none");
+  });
+
+  // Karoolian
+  $(".btnpopupkaroolian").on("click", function () {
+    $(".popupkaroolian").css("display", "flex");
+  });
+
+  $(".popupkaroolian .exit").on("click", function () {
+    $(".popupkaroolian").css("display", "none");
+  });
+
+  // matthieu
+  $(".btnpopupmatthieu").on("click", function () {
+    $(".popupmatthieu").css("display", "flex");
+  });
+
+  $(".popupmatthieu .exit").on("click", function () {
+    $(".popupmatthieu").css("display", "none");
+  });
+
+  // dkr
+  $(".btnpopupdkr").on("click", function () {
+    $(".popupdkr").css("display", "flex");
+  });
+
+  $(".popupdkr .exit").on("click", function () {
+    $(".popupdkr").css("display", "none");
+  });
+
+  // fox
+  $(".btnpopupfox").on("click", function () {
+    $(".popupfox").css("display", "flex");
+  });
+
+  $(".popupfox .exit").on("click", function () {
+    $(".popupfox").css("display", "none");
+  });
+
+  // jeday
+  $(".btnpopupjeday").on("click", function () {
+    $(".popupjeday").css("display", "flex");
+  });
+
+  $(".popupjeday .exit").on("click", function () {
+    $(".popupjeday").css("display", "none");
+  });
+
+  //Prop
+  $(".btnpopupprop").on("click", function () {
+    $(".popupprop").css("display", "flex");
+  });
+
+  $(".popupprop .exit").on("click", function () {
+    $(".popupprop").css("display", "none");
+  });
+
+  //Jaja
+  $(".btnpopupjaja").on("click", function () {
+    $(".popupjaja").css("display", "flex");
+  });
+
+  $(".popupjaja .exit").on("click", function () {
+    $(".popupjaja").css("display", "none");
+  });
+
+  //Alice
+  $(".btnpopupalice").on("click", function () {
+    $(".popupalice").css("display", "flex");
+  });
+
+  $(".popupalice .exit").on("click", function () {
+    $(".popupalice").css("display", "none");
+  });
+
+  //Lina
+  $(".btnpopuplina").on("click", function () {
+    $(".popuplina").css("display", "flex");
+  });
+
+  $(".popuplina .exit").on("click", function () {
+    $(".popuplina").css("display", "none");
+  });
+
+  //Jess
+  $(".btnpopupjess").on("click", function () {
+    $(".popupjess").css("display", "flex");
+  });
+
+  $(".popupjess .exit").on("click", function () {
+    $(".popupjess").css("display", "none");
+  });
+
+  //Marion
+  $(".btnpopupmarion").on("click", function () {
+    $(".popupmarion").css("display", "flex");
+  });
+
+  $(".popupmarion .exit").on("click", function () {
+    $(".popupmarion").css("display", "none");
+  });
+
+  //Kraquote
+  $(".btnpopupkraquote").on("click", function () {
+    $(".popupkraquote").css("display", "flex");
+  });
+
+  $(".popupkraquote .exit").on("click", function () {
+    $(".popupkraquote").css("display", "none");
+  });
+
+  //Alexis
+  $(".btnpopupalexis").on("click", function () {
+    $(".popupalexis").css("display", "flex");
+  });
+
+  $(".popupalexis .exit").on("click", function () {
+    $(".popupalexis").css("display", "none");
+  });
+
+  //Nelly
+  $(".btnpopupnelly").on("click", function () {
+    $(".popupnelly").css("display", "flex");
+  });
+
+  $(".popupnelly .exit").on("click", function () {
+    $(".popupnelly").css("display", "none");
+  });
+
+  //Julien
+  $(".btnpopupjulien").on("click", function () {
+    $(".popupjulien").css("display", "flex");
+  });
+
+  $(".popupjulien .exit").on("click", function () {
+    $(".popupjulien").css("display", "none");
+  });
+
+  //superfiix
+  $(".btnpopupsuperfiix").on("click", function () {
+    $(".popupsuperfiix").css("display", "flex");
+  });
+
+  $(".popupsuperfiix .exit").on("click", function () {
+    $(".popupsuperfiix").css("display", "none");
+  });
+
+  //Melio
+  $(".btnpopupmelio").on("click", function () {
+    $(".popupmelio").css("display", "flex");
+  });
+
+  $(".popupmelio .exit").on("click", function () {
+    $(".popupmelio").css("display", "none");
+  });
+
+  //Tsay
+  $(".btnpopuptsay").on("click", function () {
+    $(".popuptsay").css("display", "flex");
+  });
+
+  $(".popuptsay .exit").on("click", function () {
+    $(".popuptsay").css("display", "none");
+  });
+
+  //thv
+  $(".btnpopupthv").on("click", function () {
+    $(".popupthv").css("display", "flex");
+  });
+
+  $(".popupthv .exit").on("click", function () {
+    $(".popupthv").css("display", "none");
+  });
+
+  // Page souvenirs
+  $(".btnplus2021, .btnplus2020").on("click", function () {
+    $(".message2021, .message2020").css("display", "flex");
+    $(".btnplus2021,.btnplus2020").css("display", "none");
+    $(".btnmoins2021, .btnmoins2020").css("display", "flex");
+  });
+
+  $(".btnmoins2021, .btnmoins2020").on("click", function () {
+    $(".message2021, .message2020").css("display", "none");
+    $(".btnplus2021, .btnplus2020").css("display", "flex");
+    $(".btnmoins2021, .btnmoins2020").css("display", "none");
+  });
+
+  // 2021
+  $(".tri_2021").on("click", function () {
+    $(".boite_2020").css("display", "none");
+    $(".tri_2020").removeClass("active");
+    $(".boite_2021").css("display", "block");
+    $(".tri_2021").addClass("active");
+  });
+
+  // 2020
+  $(".tri_2020").on("click", function () {
+    $(".boite_2021").css("display", "none");
+    $(".tri_2021").removeClass("active");
+    $(".boite_2020").css("display", "block");
+    $(".tri_2020").addClass("active");
+  });
+
+  // PHOTOS
+
+  // 2022
+  $(".tri_photos_2022").on("click", function () {
+    $(".photos_2021, .photos_2019 ").css("display", "none");
+    $(".tri_photos_2021, .tri_photos_2019").removeClass("active");
+    $(".photos_2022").css("display", "flex");
+    $(".tri_photos_2022").addClass("active");
+  });
+
+  // 2021
+  $(".tri_photos_2021").on("click", function () {
+    $(".photos_2022, .photos_2019 ").css("display", "none");
+    $(".tri_photos_2022, .tri_photos_2019").removeClass("active");
+    $(".photos_2021").css("display", "flex");
+    $(".tri_photos_2021").addClass("active");
+  });
+
+  // 2019
+  $(".tri_photos_2019").on("click", function () {
+    $(".photos_2022, .photos_2021 ").css("display", "none");
+    $(".tri_photos_2022, .tri_photos_2021").removeClass("active");
+    $(".photos_2019").css("display", "flex");
+    $(".tri_photos_2019").addClass("active");
+  });
+
+});
